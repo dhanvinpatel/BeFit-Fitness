cb3d648b7dddeac729eed76787e46c8e44f8ab50
Dhanvin Patel <dhanvin.patel0203@gmail.com>
2022-01-19 23:09:29 -0500

    Disable value scrolling for contact form input

--- js/code.js
+++ js/code.js
@@ -1,3 +1,4 @@
+// Open navigation bar.
 function openNavigation() {
     document.getElementById("sideNavigation").style.width = "100%";
     document.getElementById("memberAccount").style.display = "flex";
@@ -5,9 +6,17 @@ function openNavigation() {
     document.body.style.height = "100%"
 }
 
+// Close navigation bar.
 function closeNavigation() {
     document.getElementById("sideNavigation").style.width = "0";
     document.getElementById("memberAccount").style.display = "none";
     document.body.style.overflow = "auto";
     document.body.style.height = "auto";
 }
+
+// Disable value scrolling behavior.
+document.addEventListener("wheel", function(event) {
+    if (document.activeElement.type == "number") {
+        document.activeElement.blur();
+    }
+})
